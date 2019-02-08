b38922283a76fe6838759f5838949b24c9d19121
JacquelineFM <jackz111111@gmail.com>
2019-02-08 15:30:07 -0600

    Métodos get y set

--- Auto.js
+++ Auto.js
@@ -1,11 +1,48 @@
-class Auto {
+export default class Auto {
+    constructor(marca, modelo, color = "blanco", kilometraje = 0) {
+        this._marca = marca;
+        this._modelo = modelo;
+        this._color = color;
+        this._kilometraje = kilometraje;
+        this._estado = "Apagado";
+    }
 
-    constructor(marca, modelo, color, kilometraje){
-        this.marca = marca;
-        this.modelo = modelo;
-        this.color = color;
-        this.kilometraje = kilometraje;
-        this.estado = "Apagado";
+    // "_" para privado
+
+    get color() {
+        return this._color;
+    }
+
+    // "get" para leer
+
+    set color(color) {
+        return this._color = color.toUpperCase();
+    }
+
+    // "set" para escribir
+
+    mostrarEstado() {
+        console.log(`El auto ${this._marca} ${this._color} del año ${this._modelo} tiene un kilometraje de ${this._kilometraje}kms y está ${this._estado}`);
+    }
+
+    encender() {
+        this._estado = "Encendido";
+        this.mostrarEstado();
+    }
+
+    apagar() {
+        this._estado = "Apagado";
+        this.mostrarEstado();
+    }
+
+    avanzar(velocidad, tiempo) {
+        if(this._estado === "Apagado") {
+            console.log('El auto no puede avanzar por que está apagado');
+        } else {
+            let distancia = velocidad * tiempo;
+            this._kilometraje = this._kilometraje + distancia;
+            this.mostrarEstado();
+        }
     }
 
 }
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hola, mundo!");
+import Auto from "./Auto.js";
+
+let auto1 = new Auto("Ford", 2000, "rojo", 100);
+let auto2 = new Auto("Nissan", 2018, "blanco");
+let auto3 = new Auto("VW", 2010);
+
+console.log(`Color ${auto1.color}`); //Lectura
+console.log(`Color ${auto2.color}`); //Lectura
+auto1.color = "Amarillo"; //Escritura
+auto2.color = "Azul"; //Escritura
+
+auto2.encender();
+auto3.encender();
+
+auto1.encender();
+auto1.avanzar(50, 2);
+auto1.apagar();
+auto1.avanzar(20, 1);
